97a70c10bf04d3b15954265738992e7b83f39284
M12545 <78020909+M12545@users.noreply.github.com>
2021-03-08 10:54:49 +0100

    Update teststkinter.py

--- teststkinter.py
+++ teststkinter.py
@@ -1,18 +1,1 @@
-import tkinter as tk
-
-window = tk.Tk()
-
-window.title("Test Tkinter")
-
-window.geometry("720x480")
-
-window.minsize(480, 480)
-
-window.config(background='#41B77F')
-
-
-label_title = Label(window, text="Bienvenue sur cette fenêtre inutile", font = ("",20), bg='#41B77F', fg='white')
-label_title.pack()
-
-
-window.mainloop()
+test branch
